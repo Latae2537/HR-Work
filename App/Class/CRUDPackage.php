4161d94129c58aa0487e1b7efcf4579c190c5c5e
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-06-27 15:44:54 +0700

    อัพโหลดไฟล์สำหรับทำ API และระบบหลังบ้านต่างๆ

---
+++ App/Class/CRUDPackage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Class\CRUD\Package;
+require_once "./PDOConnection.php";
+
+
+use \Class\Connection\PDOConnection;
+
+class CRUDPackage extends PDOConnection
+{
+
+    public function Insert(){
+
+    }
+    public function Update(){
+
+    }
+    public function Delete(){
+
+    }
+    
+    public function ValiadateInput(){
+
+    }
+    public function CSRF(){
+
+    }
+
+}
+?>
---
+++ App/Class/PDOConnection.php
@@ -0,0 +1,58 @@
+<?php
+    namespace Class\Connection;
+    use \Exception;
+    use \PDO;
+use PDOException;
+
+    class PDOConnection 
+    {
+        private $_hostname = "";
+        private $_username = "";
+        private $_password = "";
+        private $_databaseName = "";
+        private $_typeDatabase = "MYSQL";
+        public $pdo;
+
+        public function __construct($connectionStatus = false)
+        {
+
+            switch ($this->_typeDatabase) {
+                case 'MYSQL':
+                        $this->OpenConnectionMysql();
+                    break;
+                case 'MSSQL':
+                        $this->OpenConnectionMssql();
+                    break;
+                default:
+                    try {
+                        throw new Exception("Not in mysql and sql server", 1);
+                    } catch (\Throwable $err) {
+                        echo "Error : ".$err->getMessage();
+                    }
+                      
+                break;
+            }
+
+        }
+        public function OpenConnectionMysql() {
+           try {
+            $pdo = new PDO("mysql:host=$this->_hostname;dbname=$this->_username;",$this->_username,$this->_password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+            throw new Exception("Error Processing Request", 1);
+           } catch (\Throwable $err) {
+                echo  $err->getMessage();
+           } 
+        }
+        public function OpenConnectionMssql() {
+            try {
+             $pdo = new PDO("mysql:host=$this->_hostname;dbname=$this->_username;",$this->_username,$this->_password);
+             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             $this->pdo = $pdo;
+             throw new Exception("Error Processing Request", 1);
+            } catch (PDOException $err) {
+                 echo  $err->getMessage();
+            } 
+         }
+    }
+?>
